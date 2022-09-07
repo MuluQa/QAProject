808db47fd8a4120a3e7394c341a357ce3be049c3
baric <baric@10.0.0.140>
2022-09-07 09:11:21 -0400

    Updated upload

--- QA Automation Project/src/testNgPackage/BeforeAfterClassTwo.java
+++ QA Automation Project/src/testNgPackage/BeforeAfterClassTwo.java
@@ -11,30 +11,46 @@ import org.testng.annotations.Test;
 public class BeforeAfterClassTwo {
 	WebDriver browserObject;
   
-  @Test
+  @Test(priority = 1)
   public void OpeningBrowserTwo() throws InterruptedException {
 	  
-	  browserObject.get("http://practice.automationtesting.in/");
+	  browserObject.get("http://practice.automationtesting.in/"); 
 	  
-	  Thread.sleep(2000);
+	  Thread.sleep(2000);	
+  }
+	 @Test(dependsOnMethods = "OpeningBrowserTwo")
+	 public void openingUrl() throws InterruptedException{
 	  
-	  System.out.println("url is "+ browserObject.getCurrentUrl());
-	  
+	   System.out.println("url is " + browserObject.getCurrentUrl());
+	   
+	   Thread.sleep(2000);
+	 
+ } 
+	@Test(dependsOnMethods = "openingUrl")
+	public void ShopButton() throws InterruptedException {
 
 	  WebElement clickElementShop = browserObject.findElement(By.xpath("//*[@id=\"menu-item-40\"]/a"));
 	  	
 	  clickElementShop.click();
 	  
 	  Thread.sleep(2000);
-	  
-	  System.out.println("Perform click on the shop button, using radiobutton method");
-
-	  
+	}
+	
+	 @Test (dependsOnMethods = "ShopButton") 
+	 
+	 public void clickHomeButton() throws InterruptedException{
+		 
 	  browserObject.findElement(By.xpath("//*[@id=\"content\"]/nav/a")).click();
 	  
 	  System.out.println("Perform click on the Home button using navigat forward method.");
 	  
+	  Thread.sleep(2000);	 
+	  }  
 	  
+	 @Test(dependsOnMethods = "clickHomeButton")
+	 
+	 public void threeSliders() throws InterruptedException {
+	 
 	  browserObject.findElement(By.xpath("//*[@id=\"n2-ss-6-arrow-next\"]/img")).click();
 	  
 	  Thread.sleep(2000);
@@ -49,6 +65,11 @@ public class BeforeAfterClassTwo {
 	  
 	  System.out.println("Three Sliders");
 	  
+	 }
+	 
+	 @Test (dependsOnMethods= "threeSliders")
+	 
+	 public void arrivalImage() throws InterruptedException {
 	  
 	  browserObject.findElement(By.xpath("//*[@id=\"text-22-sub_row_1-0-1-1-0\"]/h2")).click();
 	  
@@ -76,23 +97,33 @@ public class BeforeAfterClassTwo {
 	  
 	  System.out.println("Cliking the Image using navigate back and forword method");
 	  
+
+	 }  
 	  
-	  browserObject.findElement(By.xpath("//*[@id=\"text-22-sub_row_1-0-2-0-0\"]/div/ul/li/a[2]")).click();
+	 @Test (dependsOnMethods = "arrivalImage")
+	 public void addingBookIntoBasket() throws InterruptedException {
 		 
-	  Thread.sleep(3000);
+	browserObject.findElement(By.xpath("//*[@id=\"text-22-sub_row_1-0-2-0-0\"]/div/ul/li/a[2]")).click();
 		 
 	  browserObject.findElement(By.xpath("//*[@id=\"text-22-sub_row_1-0-2-2-0\"]/div/ul/li/a[2]")).click();
 		  
 	  System.out.println("Adding books into the basket");
-	  Thread.sleep(3000);
 	  
-	  	browserObject.findElement(By.xpath("//*[@id=\"text-22-sub_row_1-0-2-0-0\"]/div/ul/li/a[1]/img")).click();
+	  Thread.sleep(6000);
 	  
+	 }
+	 
+	 @Test(dependsOnMethods = "addingBookIntoBasket" )
+	 
+	 public void clickingOnDescription() throws InterruptedException {
+	 	
+		 browserObject.findElement(By.xpath("//*[@id=\"text-22-sub_row_1-0-2-0-0\"]/div/ul/li/a[1]/img")).click();
+		  
 		  browserObject.navigate().forward();
 		  
-		  Thread.sleep(3000);
+		  Thread.sleep(6000);
 		  
-		  browserObject.findElement(By.xpath("//*[@id=\"product-160\"]/div[3]/ul/li[1]")).click();
+		  browserObject.findElement(By.xpath("//*[@id=\"product-160\"]/div[3]/ul/li[1]/a")).click();
 		  
 		  browserObject.navigate().back();
 		 
@@ -100,11 +131,9 @@ public class BeforeAfterClassTwo {
 		  
 		  browserObject.navigate().forward();
 		  
-		  browserObject.findElement(By.xpath("//*[@id=\"product-165\"]/div[3]/ul/li[1]")).click();
-		 
-		 System.out.println("Clicking in description tab");
-		 
-		 Thread.sleep(6000);
+		  browserObject.findElement(By.xpath("//*[@id=\"product-165\"]/div[3]/ul/li[1]/a")).click();
+		  
+		  Thread.sleep(6000);
   }
   
 		 @BeforeTest
