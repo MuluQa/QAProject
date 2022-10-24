47298d82d2b9b540898c2d2aa2ce46952fb92a4a
baric <baric@10.0.0.140>
2022-10-23 20:20:18 -0400

    Take a look please.

--- JavaProject/src/seleniumPackageTwo/TakeScreenShotPractice.java
+++ JavaProject/src/seleniumPackageTwo/TakeScreenShotPractice.java
@@ -2,39 +2,48 @@ package seleniumPackageTwo;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 public class TakeScreenShotPractice {
 
-		WebDriver driver;
-		public static void main(String[] args) {
-			System.setProperty("webdriver.chorome.driver","C:\\Program Files\\chromedriver.exe");
-			
-			WebDriver driver = new ChromeDriver();
-			
-			driver.manage().window().maximize();
-			
-			driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-			
-			driver.get("https://www.itlearn360.com");
-			
-			File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-
-			try {
-				FileUtils.copyFile(screenshot,new File("C:webdriver.chorome.driver","C:\\Program Files\\chromedriver.exe"));
-			}
-				catch (IOException e) {
-			        System.out.println(e.getMessage());
-		    }
-		
-		    
-		driver.close();
-
+				    public static void main(String[] args) throws InterruptedException {
+			        // TODO Auto-generated method stub
+			        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jayant\\Pictures\\qatraininimages\\chromedriver_win32\\chromedriver.exe");
+			        WebDriver browserObject = new ChromeDriver();
+			        browserObject.manage().window().maximize();
+			        browserObject.get("http://demo.itlearn360.com/");
+			        browserObject.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS) ;
+			        
+			        EventFiringWebDriver eventDriver = new EventFiringWebDriver(browserObject).register(new AbstractWebDriverEventListener() {
+			            
+			              public void onException(Throwable throwable, WebDriver browser) {
+			                // Take the screenshot using the Webdriver.
+			                File screen = ((TakesScreenshot)browserObject).getScreenshotAs(OutputType.FILE);
+			                // Now you can copy the screenshot somewhere on your system.
+			                try {
+			                    FileUtils.copyFile(screen, new File("C:\\project\\exception.png"));
+			                } catch (IOException e) {
+			                    // TODO Auto-generated catch block
+			                    e.printStackTrace();
+			                }
+			              }
+			            });
+			            try {
+			                eventDriver.findElement(By.xpath("//*[@id=\"loginlabel\"]/div[1]/p/a")).click();
+			            } catch (NoSuchElementException e) {   
+			              // Triggering point for the <onException> event.
+			                eventDriver.close();
+			            }
+			        browserObject.quit();
 	}
 
 }
